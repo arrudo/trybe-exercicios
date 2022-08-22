c7addf01cdb277506da63559573bf04361b7bd8a
André <andrearruda068@gmail.com>
2022-08-22 15:31:27 -0300

    ex_11 feito

--- Modulo_01/bloco_04/dia_02/ex_11/scripts.js
+++ Modulo_01/bloco_04/dia_02/ex_11/scripts.js
@@ -1,9 +1,13 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-impares = []
+let numbers = [22,4];
+impares = 0
 
 for (i=0; i < numbers.length; i+=1){
-    if(numbers[i] % 2 ==! 0){
-        soma += numbers
+    if(numbers[i] % 2 != 0){
+        impares +=1
     }
-    //INCOMPLETO
+}
+if(impares > 0){
+    console.log(impares)
+}else if(impares == 0){
+    console.log('nenhum valor ímpar encontrado')
 }
