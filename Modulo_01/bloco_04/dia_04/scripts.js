4ecd0f8a563e2c0922848fbe128957dc2226a2b3
André <andrearruda068@gmail.com>
2022-08-24 00:07:20 -0300

    ex_04 feito

---
+++ Modulo_01/bloco_04/dia_04/scripts.js
@@ -0,0 +1,15 @@
+function adicao(a, b){
+    return a + b;
+}
+function subtracao(a, b){
+    return a - b;
+}
+function multiplicacao(a, b){
+    return a * b;
+}
+function divisao(a, b){
+    return a / b;
+}
+function modulo(a, b){
+    return a % b;
+}
